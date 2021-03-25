baadaa7b4fbd800b4ca6e131b1873bfa1938245b
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-25 16:58:50 +0100

    creata la funzione per verifica palindromo

--- js/script.js
+++ js/script.js
@@ -6,13 +6,29 @@ var parolaUtente = prompt("Inserisci una parola");
 console.log('parolautente: ', parolaUtente);
 
 // scompongo la parola inserita dall'utente 
-for (var i = 0; i < parolaUtente.length; i++) {
-    var letteraParolaUtente = parolaUtente[i];
-    console.log(letteraParolaUtente);
-}
+// for (var i = 0; i < parolaUtente.length; i++) {
+//     var letteraParolaUtente = parolaUtente[i];
+//     console.log(letteraParolaUtente);
+// }
 
-// ribalto la parola dell'utente 
-for (var j = parolaUtente.length; j > 0; j--) {
-    var parolaRibaltata = parolaUtente[j];
+// ribalto la parola dell'utente
+var parolaRibaltata;
+
+for (var j = parolaUtente.length - 1; j >= 0; j--) {
+    parolaRibaltata = parolaUtente[j];
     console.log(parolaRibaltata);
 }
+
+// creo funzione per confrontare la parola dell'utente con quella ribaltata 
+function verificaPalindromo (dato1, dato2) {
+    if (parolaUtente = parolaRibaltata) {
+        console.log("è palindromo")
+    } else {
+        console.log("non è palindromo")
+    }
+
+    return risultato;
+}
+
+var output = verificaPalindromo(parolaUtente, parolaRibaltata);
+console.log(output);
